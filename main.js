094139e2e6627533a9da020401c15b1c109638f5
Alex <1834551900@qq.com>
2024-05-21 16:13:21 +0800

    完成了 home首页的开发

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+// 导入网络请求的包（由于平台的限制，小程序项目中不支持 axios，而且原生的 wx.request() API 功能较为简单，不支持拦截器等全局定制的功能。
+// 因此，建议在 uni-app 项目中使用 @escook/request-miniprogram 第三方包发起网络数据请求。地址：https://www.npmjs.com/package/@escook/request-miniprogram）
+// 注意：导入模块别放在 【ifndef VUE3    endif】 中 ，会导致解析失败
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+// 请求根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+// 配置请求拦截器
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title:'数据加载中...'
+	})
+}
+// 配置响应拦截器
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+
+// 封装弹框的方法，需要弹框提示的地方不需要再多次写重复的代码
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500){
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
 
 // #ifndef VUE3
 import Vue from 'vue'
