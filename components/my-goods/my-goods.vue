7dec763c4599e5d239bf34b941c9386df264c663
Alex <1834551900@qq.com>
2024-05-27 11:38:01 +0800

    完成了goodlist商品列表功能

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,67 @@
+<template>
+	<view class="goods-item">
+		<view class="goods-item-left">
+			<image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+		</view>
+		<view class="goods-item-right">
+			<view class="goods-name">{{ goods.goods_name}}</view>
+			<view class="goods-info-box">
+				<text class="goods-price">￥{{ Number(goods.goods_price).toFixed(2)}}</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		data() {
+			return {
+				// 默认的空图片
+				// defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png
+				defaultPic: '/static/images/celebrity-default-medium.png'
+			};
+		},
+		// 定义 props 属性，用来接收外界传递到当前组件的数据
+		props:{
+			// 商品的信息对象
+			goods:{
+				type: Object,
+				default: {}
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.goods-item{
+		display: flex;
+		padding: 10px 5px;
+		border-bottom: 1px solid #f0f0f0;
+		
+		.goods-item-left{
+			
+			.goods-pic{
+				width: 100px;
+				height: 100px;
+				display: block;
+			}
+		}
+		.goods-item-right{
+			margin-left: 5px;
+			display: flex;
+			flex-direction: column; // 纵向布局
+			justify-content: space-between; // 两端排列
+			
+			.goods-name{
+				font-size: 13px;
+			}
+			.goods-info-box{
+				.goods-price{
+					color: #c00000;
+					font-size: 16px;
+				}
+			}
+		}
+	}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,11 @@
 <template>
 	<view>
-		
+		<view class="goods-list">
+			<view v-for="(goods,i) in goodsList" :key="i" @click="gotoDetail(goods)">
+				<!-- 为 my-goods 组件动态绑定 goods 属性的值 -->
+				<my-goods :goods="goods"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +13,86 @@
 	export default {
 		data() {
 			return {
-				
+				// 请求参数对象
+				queryObj: {
+					// 查询关键词
+					query: '',
+					// 商品分类Id
+					cid: '',
+					// 页码值
+					pagenum: 1,
+					// 每页显示多少条数据
+					pagesize: 10
+				},
+				// 商品列表的数据
+				goodsList: [],
+				// 总数量，用来实现分页
+				total: 0,
+				// 是否正在请求数据
+				isloading: false
 			};
+		},
+		onLoad(options){
+			// 将页面参数转存到 this.queryObj 对象中
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			
+			// 调用获取商品列表数据的方法
+			this.getGoodsList()
+		},
+		methods:{
+			async getGoodsList(cb){
+				// ** 打开节流阀
+				this.isloading = true
+				
+				// 发起请求
+				const { data: res } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				// ** 关闭节流阀
+				this.isloading = false
+				
+				// 只要cb函数存在,只要数据请求完毕，就立即按需调用 cb 回调函数
+				cb && cb()
+				
+				if (res.meta.status !== 200) return uni.$showMsg()
+				// 为数据赋值：通过展开运算符的形式，进行新旧数据的拼接
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+			},
+			// 点击跳转到商品详情页面
+			gotoDetail(item) {
+			  uni.navigateTo({
+			    url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+			  })
+			}
+		},
+		// 触底的刷新数据事件
+		onReachBottom() {
+			// 判断是否正在请求其它数据，如果是，则不发起额外的请求
+			if (this.isloading) return
+			
+			// 判断是否还有下一页数据
+			// if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+			if (this.goodsList.length === this.total) return uni.$showMsg('数据加载完毕！')
+			
+			// 让页码值自增 +1
+			this.queryObj.pagenum += 1
+			// 重新获取列表数据
+			this.getGoodsList()
+		},
+		// 下拉刷新的事件
+		onPullDownRefresh() {
+		  // 1. 重置关键数据
+		  this.queryObj.pagenum = 1
+		  this.total = 0
+		  this.isloading = false
+		  this.goodsList = []
+		
+		  // 2. 重新发起请求,传入关闭下拉刷新函数,数据加载完毕后马上关闭下拉刷新状态,不然会一直处于下拉刷新的状态.
+		  this.getGoodsList(() => uni.stopPullDownRefresh())
 		}
 	}
 </script>
 
 <style lang="scss">
-
+	
 </style>
